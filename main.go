f5de8dc8d59c4e274da518545643a584a435defa
monea <monea@adobe.com>
2025-11-17 16:51:06 +0200

    Adds start/shutdown methods and server struct Adds channels for server thread and interrupt signals Adds flag parsing

--- main.go
+++ main.go
@@ -1,22 +1,78 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"io"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
+
+type gracefulServer struct {
+	httpServer *http.Server
+	listener   net.Listener
+}
 
 func handle(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Incoming request: %v", r)
 	io.WriteString(w, "hallo fraulain!\n")
 }
 
+func newServer(port string) *gracefulServer {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handle)
+	httpServer := &http.Server{Addr: port, Handler: mux}
+	return &gracefulServer{httpServer: httpServer}
+}
+
+func (server *gracefulServer) start() error {
+	listener, err := net.Listen("tcp", server.httpServer.Addr)
+	if err != nil {
+		return err
+	}
+	server.listener = listener
+	go server.httpServer.Serve(server.listener)
+	log.Default().Printf("Server now listening on %s\n", server.httpServer.Addr)
+	return nil
+}
+
+func (server *gracefulServer) shutdown() error {
+	if server.listener != nil {
+		err := server.listener.Close()
+		if err != nil {
+			return err
+		}
+	}
+	log.Default().Println("Server shutting down...")
+	return nil
+}
+
 func main() {
-	port := 8080
+	port := "8080"
 
-	http.HandleFunc("/", handle)
+	flag.StringVar(&port, "port", "8080", "./server -port 8080")
+	flag.Parse()
 
-	log.Printf("Starting server on localhost:%d\n", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+	done := make(chan bool, 1)
+	interrupts := make(chan os.Signal, 1)
+	signal.Notify(interrupts, syscall.SIGTERM, syscall.SIGINT)
+
+	server := newServer(port)
+
+	err := server.start()
+	if err != nil {
+		log.Fatalf("Error starting server - %v\n", err)
+	}
+
+	go func() {
+		sig := <-interrupts
+		log.Default().Printf("Received shutdown signal - %v\n", sig)
+		server.shutdown()
+		done <- true
+	}()
+
+	<-done
 }
